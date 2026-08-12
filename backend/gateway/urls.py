e5538151aace39c177b501f056f7ace4ae3d2e86
مهرشاد <meh83valiz@proton.me>
2026-08-13 00:41:23 +0330

    Add trailer preview API to backend

--- backend/gateway/urls.py
+++ backend/gateway/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import TrendingView, SearchView, EpisodesView
+from .views import TrendingView, SearchView, EpisodesView, VideoStreamInfoView
 
 urlpatterns = [
     path('api/trending/', TrendingView.as_view(), name='api-trending'),
     path('api/search/', SearchView.as_view(), name='api-search'),
     path('api/episodes/', EpisodesView.as_view(), name='api-episodes'),
+    path(
+        'api/video/<str:video_id>/',
+        VideoStreamInfoView.as_view(),
+        name='video_info',
+    ),
 ]
--- backend/gateway/views/__init__.py
+++ backend/gateway/views/__init__.py
@@ -2,3 +2,4 @@ from .details import SeriesDetailedView
 from .episodes import EpisodesView
 from .trending import TrendingView
 from .search import SearchView
+from .video import VideoScraper, VideoStreamInfoView
